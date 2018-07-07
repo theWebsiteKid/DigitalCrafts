365c51cd45675d6f94941ed635720371fe86edd1
Xavier Duncan <xduncan@protonmail.com>
2018-07-06 22:34:38 -0400

    commit all digital crafts projects

---
+++ my-web-project.sh
@@ -0,0 +1,8 @@
+mkdir my-web-project
+cd my-web-project
+mkdir stylesheets
+touch stylesheets/styles.css
+mkdir scripts
+touch scripts/scripts.js
+touch index.html
+code ./
---
+++ object-101/oop.py
@@ -0,0 +1,95 @@
+class Contact(object):
+    def __init__(self, params):
+        # self.first_name = first
+        # self.middle_name = middle
+        # self.last_name = last
+        # self.phone = phone
+        self.attributes = params
+
+    def first(self):
+        return self.get_attr('first')
+
+    def middle(self):
+        return self.get_attr('middle')
+
+    def last(self):
+        return self.get_attr('last')
+
+    def phone(self):
+        return self.get_attr('phone')
+
+    def as_string(self):
+        return "%s %s %s: %s" % (self.first(), self.middle(), self.last(), self.phone())
+
+    def get_attr(self, attr):
+        if attr in self.attributes:
+            return self.attributes[attr]
+        else:
+            return ''
+
+contact = Contact({
+    'first': 'Xavier',
+    'middle': 'Marcus',
+    'last': 'Duncan',
+    'phone': '404618LABS'
+})
+
+class Phonebook(object):
+    #Stores list of Contact
+    def __init__(self):
+        self.contacts = []
+    
+    def add(self, contact):
+        self.contacts.append(contact)
+
+    def as_string(self):
+        string = ''
+        for contact in self.contacts:
+            string += contact.as_string() + '\n'
+
+        return string
+
+    # def remove():
+    #     ...
+
+    # def sort():
+    #     ...
+
+    # def lookup():
+    #     ...
+
+    # def lookup_email():
+    #     ...
+
+    # def lookup_name():
+    #     ...
+
+phonebook = Phonebook()
+contact = Contact({
+    'first': 'Xavier',
+    'middle': 'Marcus',
+    'last': 'Duncan',
+    'phone': '404618LABS'
+})
+
+phonebook.add(contact)
+print phonebook.as_string()
+
+# Pygame Inherentance
+# Build a game utilizing inherentance. Turn based text games!!!
+class Character(object):
+    def __init__(self):
+        self.alive = True
+
+    def kill(self):
+        self.alive = False
+
+class Hero(Character):
+    def greet(self):
+        print "Hey there"
+
+class Enemy(Character):
+    def greet(self):
+        print "Hssss"
+
+enemy = Enemy()
