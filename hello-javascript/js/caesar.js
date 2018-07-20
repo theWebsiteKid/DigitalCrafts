3b22695183532b56abebe9248685fa4ae3a3470a
Xavier Duncan <xduncan@pm.me>
2018-07-20 12:47:11 -0400

    major refactor to caesar.js

---
+++ hello-javascript/js/caesar.js
@@ -0,0 +1,17 @@
+// init new list (map) with alphabet
+var map = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// init cipher function
+var cipher = function (input = 'Hello World') {
+    // init phrase variable with
+    // lowercased input string
+    var inputLowerCase = input.toLowerCase();
+    // init an array of letters
+    // using split phrase string
+    var letters = inputLowerCase.split('');
+    // test it!
+    // 1) an array of each char from input?
+    // 2) are letters lowercased?
+    console.log(letters);
+};
+// call cipher function
+cipher();
